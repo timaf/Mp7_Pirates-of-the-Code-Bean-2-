1d25a3078109c7b64cd6b3de08bb5bd1e7f92124
timaf <lo_sa_na@yahoo.com>
2018-03-29 12:53:06 +0200

    start

---
+++ jack_sparrow/src/main/java/at/refugeescode/jack_sparrow/JackSparrowApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.jack_sparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackSparrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackSparrowApplication.class, args);
+	}
+}
---
+++ tia_Dalma/src/main/java/at/refugeescode/tia_Dalma/TiaDalmaApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.tia_Dalma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaDalmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiaDalmaApplication.class, args);
+	}
+}
